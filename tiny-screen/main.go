52d21b708c866cbe9c0da47f66496ad628d6a713
akhilesh-dev <akhilesh@yajtechnologies.com>
2025-07-26 17:29:55 +0545

    retry

--- tiny-screen/main.go
+++ tiny-screen/main.go
@@ -1,20 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"time"
 	tf "tiny-screen/ffmpeg-core"
 )
 
 func main() {
-	// encodeHls()
+	encodeHlsRetry(3)
 	serve()
 }
 
-func encodeHls() {
+func encodeHlsRetry(maxRetry int) error{
 	x11 := tf.TinyFfmpegX11{}
-	if err := x11.HlsEncode("input-samples/sample-video.mp4", "", true); err != nil {
-		log.Fatal(err)
+	var err error
+	for attempt := 1; attempt <= maxRetry; attempt++ {
+		fmt.Printf("Attempt %d of %d", attempt, maxRetry)
+		err = x11.HlsEncode("input-samples/sample-video.mp4", "", true)
+		if err == nil {
+			return nil
+		}
+		fmt.Printf("FFmpeg failed: %v\n", err)
+		if attempt < maxRetry {
+			time.Sleep(time.Second * 2)
+		}
 	}
+	return fmt.Errorf("all %d ffmpeg attempts failed: last error: %w", maxRetry, err)
 }
 
 func recordScreen() {
